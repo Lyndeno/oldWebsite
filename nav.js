526597a6a9346a795d3552b81346303cecb2ce03
Lyndeno <lyndeno@gmail.com>
2018-09-17 20:35:31 -0600

    Did some header work

--- nav.js
+++ nav.js
@@ -21,12 +21,12 @@ index = 0;
 
 var html = `<ul>`
 
-while(index < 3){
-    if(pages[index][2] == true){
-        html += `<li><a class="active" href="${pages[index][1]}">${pages[index][0]}</li>`;
+while(index < 3) {
+    if (pages[index][2] == true) {
+        html += `<li><a class="active" href="${pages[index][1]}">${pages[index][0]}</a></li>`;
     }
-    else{
-        html += `<li><a href="${pages[index][1]}">${pages[index][0]}</li>`;
+    else if (pages[index][2] == false) {
+        html += `<li><a href="${pages[index][1]}">${pages[index][0]}</a></li>`;
     }  
     index++;
 }
