046b0d8c48a5b28371e0e6dfe39ba4ca28d56555
Lyndon Sanche <lyndeno@gmail.com>
2019-01-16 11:42:41 -0700

    added 404

---
+++ errors/404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <meta name="viewport" content="width=device-width, initial-scaled=1.0">
+    <meta name="theme-color" content="#f10202">
+    <head>
+        <title>Oops!</title>
+    </head>
+
+    <body>
+        <div>
+            <h1>Oops! - 404 Not Found</h1>
+            <p>I couldn't find the page that your looking for.</p>
+        </div>
+    </body>
+</html>
