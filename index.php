3d22d6f898fb37b3eca18253987d3e10710cad54
Lyndon Sanche <lyndeno@gmail.com>
2019-01-16 10:10:25 -0700

    More code cleanup

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php $title = 'Home'; ?>
+<?php $currentPage = 'Home'; ?>
 
 <?php include('head.php'); ?>
 <?php include('navbar.php'); ?>
-    <body>
-        <script src="head.js" activePage="Home"></script>
-        <div class="content">
-        <title>Lyndon Sanche</title>
+
+<script src="head.js" activePage="Home"></script>
+<div class="content">
+    <title>Lyndon Sanche</title>
 
 
-        <h2>Home</h2>
-        <p style="padding-bottom: 1500px">
-            Hello, my name is Lyndon Sanche and this is my website.
-        </p>
+    <h2>Home</h2>
+    <p style="padding-bottom: 1500px">
+    Hello, my name is Lyndon Sanche and this is my website.
+    </p>
         
-        </div>
-    </body>
+</div>
+</body>
 </html>
--- navbar.php
+++ navbar.php
@@ -1,10 +1,11 @@
 <header>
     <div id="navbar">
-        <a href="index.php">Home</a></li>
-        <a href="about.php">About</a></li>
-        <a href="contact.php">Contact</a></li>
-        <a href="craft.php">Craft</a></li>
-        <a href="tech.php">Tech Help</a></li>
+        <a href="index.php" <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>>Home</a></li>
+        <a href="about.php" <?php if ($currentPage === 'About') {echo 'class="active"';} ?>>About</a></li>
+        <a href="contact.php" <?php if ($currentPage === 'Contact') {echo 'class="active"';} ?>>Contact</a></li>
+        <a href="craft.php" <?php if ($currentPage === 'Craft') {echo 'class="active"';} ?>>Craft</a></li>
+        <a href="tech.php" <?php if ($currentPage === 'Tech') {echo 'class="active"';} ?>>Tech Help</a></li>
     </div>
     <script src="navbar.js"></script>
 </header>
+<body>
