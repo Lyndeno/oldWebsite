25d948fbf4e702e6b754741d433f1582ac9717d5
BuildTools <unconfigured@null.spigotmc.org>
2019-01-21 19:16:20 -0700

    added special page

---
+++ jazzykins.php
@@ -0,0 +1,17 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Jazzykins</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/errors/error.css">
+</head>
+<body style="background: pink">
+    <div style="background: #fdeeea">
+        <h1>Happy Birthday my Jazzykins!!!!!!</h1>
+        <p>I hope you have a great day today :)</p>
+        <p>I like you :)</p>
+    </div>
+</body>
+</html>
