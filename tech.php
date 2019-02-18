944d098a1535cd89f604124fc78f644765dbbd98
BuildTools <unconfigured@null.spigotmc.org>
2019-02-17 21:55:37 -0700

    testing more

--- tech.php
+++ tech.php
@@ -5,11 +5,11 @@
 <?php include('navbar.php'); ?>
 
 <div class="content">
-    <p>
     <ul>
         <li>Test1</li>
         <li>Test2</li>
     </ul>
+    <p>
     If you need help with something I can probably help you with it. I don't have any set
         rates so just <a href="contact.php">contact me</a> and we can discuss the issue.
     </p>
