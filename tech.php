ea1ae53758d4815d26498e766eb53e86cd12cf27
BuildTools <unconfigured@null.spigotmc.org>
2019-02-17 22:10:00 -0700

    added information

--- tech.php
+++ tech.php
@@ -5,13 +5,23 @@
 <?php include('navbar.php'); ?>
 
 <div class="content">
+    
+    <p>
+        I am offering a tech help service for anyone who needs assistance witht their systems.
+        I have no set fees so you can contact me at <a href="mailto:tech@lyndeno.ca">tech@lyndeno.ca</a>.
+    </p>
+    <p>I have knowledge and can assist in the following systems:</p>
     <ul>
-        <li>Test1</li>
-        <li>Test2</li>
+        <li>Windows Computers</li>
+        <li>MacOS Computers</li>
+        <li>Most Linux systems</li>
+        <li>Apple iPhone</li>
+        <li>Android Phones</li>
+        <li>Windows Phones</li>
     </ul>
     <p>
-    If you need help with something I can probably help you with it. I don't have any set
-        rates so just <a href="contact.php">contact me</a> and we can discuss the issue.
+        With most of these systems I can also remote control your system which makes it easier to assist. We can discuss times for that if needed.
+        I can help with many problems regarding these systems, these issues could include viruses, slow performance, as well as other issues, or if you just need help doing something.
     </p>
 </div>
 </body>
