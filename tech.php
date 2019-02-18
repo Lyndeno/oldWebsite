4147167051ff3a2b8ae00795b4f49221139435b5
Lyndon Sanche <lyndeno@gmail.com>
2019-02-18 10:21:19 -0700

    Fixed grammar errors

--- tech.php
+++ tech.php
@@ -7,9 +7,9 @@
 <div class="content">
     
     <p>
-        I am offering a tech help service for anyone who needs assistance witht their systems.
+        I am offering a tech help service for anyone who needs assistance with their systems.
         I have no set fees so you can contact me at <a href="mailto:tech@lyndeno.ca">tech@lyndeno.ca</a>
-        (Make sure you check your spam folder for replies, this email is new and most services will throw it in to the spam folder).
+        (Make sure you check your spam folder for replies; this email is new and most services will throw it in to the spam folder).
     </p>
     <p>I have knowledge and can assist in the following systems:</p>
     <ul>
@@ -22,7 +22,7 @@
     </ul>
     <p>
         With most of these systems I can also remote control your system which makes it easier to assist. We can discuss times for that if needed.
-        I can help with many problems regarding these systems, these issues could include viruses, slow performance, as well as other issues, or if you just need help doing something.
+        I can help with many problems regarding these systems; these issues could include viruses, slow performance, as well as other problems, or if you just need help in other areas.
     </p>
 </div>
 </body>
