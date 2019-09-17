732f1cf8c5d73a7e0048528554c62cc94e648426
Lyndon Sanche <lyndeno@gmail.com>
2019-09-17 12:07:43 -0600

    Removed the craft link

--- navbar.php
+++ navbar.php
@@ -3,7 +3,6 @@
         <a href="index.php" <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>>Home</a></li>
         <a href="about.php" <?php if ($currentPage === 'About') {echo 'class="active"';} ?>>About</a></li>
         <a href="contact.php" <?php if ($currentPage === 'Contact') {echo 'class="active"';} ?>>Contact</a></li>
-        <a href="craft.php" <?php if ($currentPage === 'Craft') {echo 'class="active"';} ?>>Craft</a></li>
         <a href="tech.php" <?php if ($currentPage === 'Tech') {echo 'class="active"';} ?>>Tech Help</a></li>
         <a href="https://github.com/Lyndeno" <?php if ($currentPage === 'Github') {echo 'class="active"';} ?>>Github</a></li>
     </div>
