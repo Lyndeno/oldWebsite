b3a12260ab070f6b8e49b72bea2c856abe651b00
Lyndon Sanche <lyndeno@gmail.com>
2019-09-24 13:11:14 -0600

    Rearranged script

--- test.php
+++ test.php
@@ -1,15 +1,16 @@
 <html>
     <link rel="stylesheet" href="newbar.css">
-    <script src="newbar.js"></script>
-    
-    <div id="navbar">
-        <a href="#default" id="logo">CompanyLogo</a>
-        <div id="navbar-right">
-            <a class="active" href="#home">Home</a>
-            <a href="#contact">Contact</a>
-            <a href="#about">About</a>
+    <head>
+        <div id="navbar">
+            <a href="#default" id="logo">CompanyLogo</a>
+            <div id="navbar-right">
+                <a class="active" href="#home">Home</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
         </div>
-    </div>
+        <script src="newbar.js"></script>
+    </head>
 
     <body>
     <h1>Test Title</h1>
