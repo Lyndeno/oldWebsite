5990e5eda62208e04482e78b71093ad768095c0f
Lyndon Sanche <lyndeno@gmail.com>
2019-09-24 13:05:05 -0600

    Newbar initial test

---
+++ test.php
@@ -0,0 +1,16 @@
+<html>
+    <link rel="stylesheet" href="newbar.css">
+    <script src="newbar.js"></script>
+    
+    <div id="navbar">
+        <a href="#default" id="logo">CompanyLogo</a>
+        <div id="navbar-right">
+            <a class="active" href="#home">Home</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    </div>
+
+    <body>
+    <body>
+</html>
