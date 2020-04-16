c37b61e1320ff2595c0b429e61daea46e0f07f27
Lyndon Sanche <lsanche@lyndeno.ca>
2020-04-16 16:03:11 -0600

    add social icon

--- head.php
+++ head.php
@@ -1,6 +1,7 @@
 <head>
     <title><?php echo($title); ?></title>
     <link rel="stylesheet" href="default.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scaled=1.0">
     <meta name="theme-color" content="bd0000">
 </head>
