752e9cb81b45ee28f0068f8dd7b69ffba31c7cfd
Lyndon Sanche <lsanche@lyndeno.ca>
2020-04-16 16:19:05 -0600

    fixed formatting

--- head.php
+++ head.php
@@ -13,9 +13,9 @@
         <a href="index.php" <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>>Home</a>
         <a href="contact.php" <?php if ($currentPage === 'Contact') {echo 'class="active"';} ?>>Contact</a>
         <a href="tech.php" <?php if ($currentPage === 'Tech') {echo 'class="active"';} ?>>Tech Help</a>
-        <a href="https://github.com/Lyndeno" <?php if ($currentPage === 'Github') {echo 'class="active"';} ?>>Github</a>
-        <a href="https://github.com/Lyndeno/pachive" <?php if ($currentPage === 'pachive') {echo 'class="active"';} ?>>pachive</a>
-        <a href="#" class="fa fa-linkedin"></a><a href="https://www.linkedin.com/in/lyndon-sanche-6544b715b/" <?php if ($currentPage === 'LinkedIn') {echo 'class="active "';} ?>>LinkedIn</a>
+        <a href="https://github.com/Lyndeno">Github</a>
+        <a href="https://github.com/Lyndeno/pachive">pachive</a>
+        <a href="https://www.linkedin.com/in/lyndon-sanche-6544b715b/" class="fa fa-linkedin"></a>
     </div>
     <script src="navbar.js"></script>
     <div class="titlehead"><h2><?php echo($title); ?></h2></div>
