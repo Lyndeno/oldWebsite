64925744ddd290128cb47c9f045348ad2f9132d9
Lyndon Sanche <lsanche@lyndeno.ca>
2020-04-16 16:43:05 -0600

    Added favicon

--- head.php
+++ head.php
@@ -4,6 +4,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scaled=1.0">
     <meta name="theme-color" content="bd0000">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
 </head>
 <header>
     <div class="header">
