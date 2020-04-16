73adfdaab4cf8d729e4c5d21bcc32fa55c720eeb
Lyndon Sanche <lsanche@lyndeno.ca>
2020-04-16 14:37:46 -0600

    New contact info design

--- contact.php
+++ contact.php
@@ -5,9 +5,15 @@
 <?php include('navbar.php'); ?>
 
 <div class="content">
-    <p style="font-family: Arial, Helvetica, sans-serif" >
+    <!--<p style="font-family: Arial, Helvetica, sans-serif" >
         Email: <a href="mailto:lsanche@ualberta.ca">lsanche@ualberta.ca</a>
-    </p>
+    </p>-->
+
+    <ul class="contactAlign">
+        <li><b>University Email</b> <a href="mailto:lsanche@ualberta.ca">lsanche@ualberta.ca</a></li>
+        <li><b>Tech Email</b> <a href="mailto:tech@lyndeno.ca">tech@lyndeno.ca</a></li>
+        <li><b>Other Email</b> <a href="mailto:lsanche@lyndeno.ca">lsanche@lyndeno.ca</a></li>
+    </ul>
 </div>         
 </body>
 </html>
