61937c5c6f246532ee67629f768c775949c6c3ea
Lyndon Sanche <lsanche@lyndeno.ca>
2020-04-16 14:41:15 -0600

    Remove bullets

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
         Email: <a href="mailto:lsanche@ualberta.ca">lsanche@ualberta.ca</a>
     </p>-->
 
-    <ul class="contactAlign">
+    <ul class="contactAlign" style="list-style-type:none">
         <li><b>University Email</b> <a href="mailto:lsanche@ualberta.ca">lsanche@ualberta.ca</a></li>
         <li><b>Tech Email</b> <a href="mailto:tech@lyndeno.ca">tech@lyndeno.ca</a></li>
         <li><b>Other Email</b> <a href="mailto:lsanche@lyndeno.ca">lsanche@lyndeno.ca</a></li>
