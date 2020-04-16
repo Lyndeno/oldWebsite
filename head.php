70d396b808233bb1dcba4564420e23dd06b8d48a
Lyndon Sanche <lsanche@lyndeno.ca>
2020-04-16 15:00:20 -0600

    Fixed navbar?

--- head.php
+++ head.php
@@ -10,12 +10,12 @@
 
     <header>
         <div id="navbar">
-            <a href="index.php" <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>>Home</a></li>
-            <a href="about.php" <?php if ($currentPage === 'About') {echo 'class="active"';} ?>>About</a></li>
-            <a href="contact.php" <?php if ($currentPage === 'Contact') {echo 'class="active"';} ?>>Contact</a></li>
-            <a href="tech.php" <?php if ($currentPage === 'Tech') {echo 'class="active"';} ?>>Tech Help</a></li>
-            <a href="https://github.com/Lyndeno" <?php if ($currentPage === 'Github') {echo 'class="active"';} ?>>Github</a></li>
-            <a href="https://github.com/Lyndeno/pachive" <?php if ($currentPage === 'pachive') {echo 'class="active"';} ?>>pachive</a></li>
+            <li><a href="index.php" <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>>Home</a></li>
+            <li><a href="about.php" <?php if ($currentPage === 'About') {echo 'class="active"';} ?>>About</a></li>
+            <li><a href="contact.php" <?php if ($currentPage === 'Contact') {echo 'class="active"';} ?>>Contact</a></li>
+            <li><a href="tech.php" <?php if ($currentPage === 'Tech') {echo 'class="active"';} ?>>Tech Help</a></li>
+            <li><a href="https://github.com/Lyndeno" <?php if ($currentPage === 'Github') {echo 'class="active"';} ?>>Github</a></li>
+            <li><a href="https://github.com/Lyndeno/pachive" <?php if ($currentPage === 'pachive') {echo 'class="active"';} ?>>pachive</a></li>
         </div>
         <script src="navbar.js"></script>
         <div class="titlehead"><h2><?php echo($title); ?></h2></div>
