1da2e6669751bcc14ad8c25aea8af7380c6ead22
Lyndon Sanche <lsanche@lyndeno.ca>
2020-04-16 15:04:09 -0600

    Fixed some more errors

--- contact.php
+++ contact.php
@@ -1,17 +1,10 @@
 <!DOCTYPE html>
 <?php $title = 'Contact'; ?>
 <?php $currentPage = 'Contact'; ?>
-
-
-
 <html>
     <?php include('head.php'); ?>
     <body>
         <div class="content">
-            <!--<p style="font-family: Arial, Helvetica, sans-serif" >
-                Email: <a href="mailto:lsanche@ualberta.ca">lsanche@ualberta.ca</a>
-            </p>-->
-
             <ul class="contactAlign" style="list-style-type:none">
                 <li><b>University Email</b> <a href="mailto:lsanche@ualberta.ca">lsanche@ualberta.ca</a></li>
                 <li><b>Tech Email</b> <a href="mailto:tech@lyndeno.ca">tech@lyndeno.ca</a></li>
--- head.php
+++ head.php
@@ -1,22 +1,22 @@
+<head>
+    <title><?php echo($title); ?></title>
+    <link rel="stylesheet" href="index.css">
     <meta name="viewport" content="width=device-width, initial-scaled=1.0">
     <meta name="theme-color" content="#f10202">
-    <head>
-        <title><?php echo($title); ?></title>
-        <link rel="stylesheet" href="index.css">
-        <div class="header">
-            <h1 class="header">Lyndon Sanche</h1>
-        </div>
-    </head>
+</head>
 
-    <header>
-        <div id="navbar">
-            <a href="index.php" <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>>Home</a>
-            <a href="about.php" <?php if ($currentPage === 'About') {echo 'class="active"';} ?>>About</a>
-            <a href="contact.php" <?php if ($currentPage === 'Contact') {echo 'class="active"';} ?>>Contact</a>
-            <a href="tech.php" <?php if ($currentPage === 'Tech') {echo 'class="active"';} ?>>Tech Help</a>
-            <a href="https://github.com/Lyndeno" <?php if ($currentPage === 'Github') {echo 'class="active"';} ?>>Github</a>
-            <a href="https://github.com/Lyndeno/pachive" <?php if ($currentPage === 'pachive') {echo 'class="active"';} ?>>pachive</a>
-        </div>
-        <script src="navbar.js"></script>
-        <div class="titlehead"><h2><?php echo($title); ?></h2></div>
-    </header>
+<header>
+    <div class="header">
+        <h1 class="header">Lyndon Sanche</h1>
+    </div>
+    <div id="navbar">
+        <a href="index.php" <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>>Home</a>
+        <a href="about.php" <?php if ($currentPage === 'About') {echo 'class="active"';} ?>>About</a>
+        <a href="contact.php" <?php if ($currentPage === 'Contact') {echo 'class="active"';} ?>>Contact</a>
+        <a href="tech.php" <?php if ($currentPage === 'Tech') {echo 'class="active"';} ?>>Tech Help</a>
+        <a href="https://github.com/Lyndeno" <?php if ($currentPage === 'Github') {echo 'class="active"';} ?>>Github</a>
+        <a href="https://github.com/Lyndeno/pachive" <?php if ($currentPage === 'pachive') {echo 'class="active"';} ?>>pachive</a>
+    </div>
+    <script src="navbar.js"></script>
+    <div class="titlehead"><h2><?php echo($title); ?></h2></div>
+</header>
