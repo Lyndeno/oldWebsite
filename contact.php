80e47684fef5690a92c4e1bc409ec5cc3582686c
Lyndon Sanche <lsanche@lyndeno.ca>
2020-04-16 14:58:24 -0600

    More structure

--- contact.php
+++ contact.php
@@ -1,20 +1,21 @@
+<!DOCTYPE html>
 <?php $title = 'Contact'; ?>
 <?php $currentPage = 'Contact'; ?>
 
 <?php include('head.php'); ?>
 
-<body>
+<html>
+    <body>
+        <div class="content">
+            <!--<p style="font-family: Arial, Helvetica, sans-serif" >
+                Email: <a href="mailto:lsanche@ualberta.ca">lsanche@ualberta.ca</a>
+            </p>-->
 
-<div class="content">
-    <!--<p style="font-family: Arial, Helvetica, sans-serif" >
-        Email: <a href="mailto:lsanche@ualberta.ca">lsanche@ualberta.ca</a>
-    </p>-->
-
-    <ul class="contactAlign" style="list-style-type:none">
-        <li><b>University Email</b> <a href="mailto:lsanche@ualberta.ca">lsanche@ualberta.ca</a></li>
-        <li><b>Tech Email</b> <a href="mailto:tech@lyndeno.ca">tech@lyndeno.ca</a></li>
-        <li><b>Other Email</b> <a href="mailto:lsanche@lyndeno.ca">lsanche@lyndeno.ca</a></li>
-    </ul>
-</div>         
-</body>
+            <ul class="contactAlign" style="list-style-type:none">
+                <li><b>University Email</b> <a href="mailto:lsanche@ualberta.ca">lsanche@ualberta.ca</a></li>
+                <li><b>Tech Email</b> <a href="mailto:tech@lyndeno.ca">tech@lyndeno.ca</a></li>
+                <li><b>Other Email</b> <a href="mailto:lsanche@lyndeno.ca">lsanche@lyndeno.ca</a></li>
+            </ul>
+        </div>         
+    </body>
 </html>
